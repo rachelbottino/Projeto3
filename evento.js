e1add469c077cc3ee2c99769e8d44b63342f11cf
chends888 <lchends@gmail.com>
2017-11-20 00:49:06 -0200

    Adicionando funções para UPDATE evento, bem cru ainda

--- evento.js
+++ evento.js
@@ -64,6 +64,65 @@ console.log(new_evento);
 });
 
 
+
+
+app.get('/modificar', function (req, res) {
+    res.sendFile('views/modevento.html' , { root : __dirname});
+ });
+
+app.post('/modificar', function(req, res) {
+    var new_evento = {
+    	nome:req.body.nome,
+        descricao:req.body.descricao,
+        data:req.body.data,
+        local:req.body.local,
+	    low_carb: req.body.low_carb,
+	    vegano : req.body.vegano,
+	    vegetariano : req.body.vegetariano,
+	    sem_glutem : req.body.sem_glutem,
+	    sem_lactose : req.body.sem_lactose,
+	    cross_fit : req.body.cross_fit,
+	    esporte_coletivo : req.body.esporte_coletivo,
+	    esporte_aventura : req.body.esporte_aventura,
+	    luta : req.body.luta,
+	    yoga : req.body.yoga
+};
+
+console.log(new_evento);
+ connection.query("INSERT INTO evento SET ?", new_evento, function (error, results, fields) {	
+    		if (error) throw error;
+    		res.redirect('/');
+  });
+});
+
+
+
+app.post('/modificar', function(req, res) {
+    var new_evento = {
+    	nome:req.body.nome,
+        descricao:req.body.descricao,
+        data:req.body.data,
+        local:req.body.local,
+	    low_carb: req.body.low_carb,
+	    vegano : req.body.vegano,
+	    vegetariano : req.body.vegetariano,
+	    sem_glutem : req.body.sem_glutem,
+	    sem_lactose : req.body.sem_lactose,
+	    cross_fit : req.body.cross_fit,
+	    esporte_coletivo : req.body.esporte_coletivo,
+	    esporte_aventura : req.body.esporte_aventura,
+	    luta : req.body.luta,
+	    yoga : req.body.yoga
+};
+
+console.log(new_evento);
+ connection.query("INSERT INTO evento SET ?", new_evento, function (error, results, fields) {	
+    		if (error) throw error;
+    		res.redirect('/');
+  });
+});
+
+
 //start Server
 app.listen(3000, function () {
     console.log('Servidor rodando na porta 3000!')
